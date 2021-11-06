964ef358f4cdd7625f5b356a3376bbd2b3b36f58
Ehsan Khosravi <ekhosr@gmail.com>
2021-11-06 12:10:30 -0400

    deploying

--- server.js
+++ server.js
@@ -15,13 +15,25 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget-tracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false
+// mongoose.connect("mongodb://localhost/budgetTracker", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+//   useFindAndModify: false
 
-});
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budgetTracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+
 
 // routes
 app.use(require("./routes/api.js"));

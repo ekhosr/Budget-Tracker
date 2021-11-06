5f803c99aba733316573b2c3d62b2bd1dc78e940
Ehsan Khosravi <ekhosr@gmail.com>
2021-11-06 10:31:28 -0400

    service-worker, db.js, manifest created

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/db.js',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+  ];
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
